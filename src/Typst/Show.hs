b49a3990e3c0d49769cc9337e575d5303c5e2283
John MacFarlane <jgm@berkeley.edu>
2025-02-28 19:26:11 -0800

    Typst.Show:  simplify and improve application of show rules.
    This still doesn't handle everything typst does.

--- src/Typst/Show.hs
+++ src/Typst/Show.hs
@@ -6,25 +6,38 @@
 
 module Typst.Show (applyShowRules) where
 
-import Control.Monad (foldM)
 import Data.Array ((!))
 import qualified Data.Map as M
 import Data.Sequence (Seq)
 import qualified Data.Sequence as Seq
 import Data.Text (Text)
 import qualified Data.Text as T
-import Text.Parsec (getState, updateState, (<|>))
+import Text.Parsec (getState, updateState)
 import qualified Text.Regex.TDFA as TDFA
 import Typst.Regex (RE (..), makeLiteralRE)
 import Typst.Syntax
 import Typst.Types
 
-import Debug.Trace
+-- import Debug.Trace
 
 applyShowRules :: Monad m => Seq Content -> MP m (Seq Content)
 applyShowRules cs = do
   rules <- evalShowRules <$> getState
-  foldM (tryShowRules rules) mempty cs
+  foldMap (tryShowRules rules) cs
+
+-- By experiment, it seems that show rules work this way:
+-- the first (i.e. most recently defined) one to match a given element
+-- are applied first.
+tryShowRules ::
+  Monad m =>
+  [ShowRule] ->
+  Content ->
+  MP m (Seq Content)
+tryShowRules rs c =
+  case rs of
+    [] -> pure $ Seq.singleton c
+    (r:rest) -> applyShowRule r c >>= foldMap (tryShowRules rest)
+-- TODO recursive applyShowRules?
 
 withoutShowRule :: Monad m => ShowRule -> MP m a -> MP m a
 withoutShowRule rule pa = do
@@ -35,49 +48,25 @@ withoutShowRule rule pa = do
   updateState $ \st -> st {evalShowRules = oldShowRules}
   pure res
 
--- By experiment, it seems that show rules work this way:
--- the first (i.e. most recently defined) one to match a given element
--- are applied first.
-tryShowRules ::
-  Monad m =>
-  [ShowRule] ->
-  Seq Content ->
-  Content ->
-  MP m (Seq Content)
-tryShowRules [] cs c = pure $ cs Seq.|> c
-tryShowRules (r@(ShowRule ident sel f) : rs) cs c = do
+applyShowRule :: Monad m => ShowRule -> Content -> MP m (Seq Content)
+applyShowRule rule@(ShowRule _ sel f) c = withoutShowRule rule $ do
   case (sel, c) of
-    (SelectString s, Txt t) ->
-      ( do
-          re <- makeLiteralRE s
-          withoutShowRule r
-            ((cs <>) <$> (replaceRegexContent re t f >>= applyShowRules))
-      )
-        <|> tryShowRules rs cs c
-    (SelectRegex re, Txt t) ->
-      ( withoutShowRule r
-          ((cs <>) <$> (replaceRegexContent re t f >>= applyShowRules))
-      )
-        <|> tryShowRules rs cs c
+    (SelectString s, Txt t) | s `T.isInfixOf` t -> do
+      re <- makeLiteralRE s
+      replaceRegexContent re t f
+    (SelectRegex re@(RE _ re'), Txt t) | not (null (TDFA.matchAll re' t)) -> do
+      replaceRegexContent re t f
     (SelectLabel s, elt@(Elt _ _ fields))
-      | Just (VLabel s') <- M.lookup "label" fields,
-        s' == s ->
-          withoutShowRule r ((cs <>) <$> (f elt >>= applyShowRules))
+      | Just (VLabel s') <- M.lookup "label" fields
+      , s' == s
+      -> f elt
     (SelectElement name fields, elt@(Elt name' _ fields'))
-      | name == name',
-        fieldsMatch fields fields' ->
-          withoutShowRule r $ (cs <>) <$> (f elt >>= applyShowRules)
-    (SelectOr _sel1 _sel2, _elt) ->
-      fail "or is not yet implemented for select"
-    (SelectAnd _sel1 _sel2, _elt) ->
-      fail "and is not yet implemented for select"
-    (SelectBefore _sel1 _sel2, _elt) ->
-      fail "before is not yet implemented for select"
-    (SelectAfter _sel1 _sel2, _elt) ->
-      fail "after is not yet implemented for select"
-    _ -> tryShowRules rs cs c
+      | name == name' && fieldsMatch fields fields'
+      -> f elt
+    (_, cont) -> pure (Seq.singleton cont)
+    -- TODO not implemented: SelectOr, SelectAnd, SelectBefore, SelectAfter
 
-fieldsMatch :: [(Identifier, Val)] -> (M.Map Identifier Val) -> Bool
+fieldsMatch :: [(Identifier, Val)] -> M.Map Identifier Val -> Bool
 fieldsMatch [] _ = True
 fieldsMatch ((k, v) : rest) m =
   ( case M.lookup k m of
